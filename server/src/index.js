dfb96cb6d3f2a8f167003c71875142dac73cf822
Ataa00 <ataa.shaqour.as@gmail.com>
2023-08-01 13:03:43 +0300

    Creating user model

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,16 @@
-console.log("Hello World.");
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get("/", (req, res) => {
+    res.status(200).send("Hello World.");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Listening on: http://localhost:${PORT}`);
+})
